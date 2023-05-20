e781a19bf04b49bd3b33a53280008a2b71370c22
codejutsu1 <codejutsu@protonmail.com>
2023-05-20 18:12:16 +0100

    added my socials

--- resources/views/assignment/index.blade.php
+++ resources/views/assignment/index.blade.php
@@ -12,4 +12,29 @@
             <a href="{{ route('projects.index') }}" class="inline-block dark:bg-gray-900 mb-5 p-4 rounded-md dark:text-gray-200">My Projects</a>
         </div>
     </div>
+
+    <div class="py-10 text-gray-200 text-center font-semibold">
+        <p>Developed by DUNU DANIEL</p>
+        <ul class="flex space-x-6 justify-center mt-2">
+            <li>
+                <a class="underline" href="mailto:danieldunu001@gmail.com" target="_blank">
+                    Email
+                </a>
+            </li>
+            <li>
+                <a class="underline" href="https://twitter.com/danieldunu" target="_blank">
+                    Twitter
+                </a>
+            </li>
+            <li>
+                <a class="underline" href="https://linkedin.com/in/daniel-dunu" target="_blank">
+                    LinkedIn
+                </a>
+            <li>
+                <a class="underline" href="https://github.com/codejutsu1" target="_blank">
+                    Github
+                </a>
+            </li>
+        </ul>
+    </div>
 </x-project-layout>
